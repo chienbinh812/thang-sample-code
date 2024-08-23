f3362a7ca5ad91583a6a1c030fd59464cedafe0f
chienbinh812 <ducthang0812@gmail.com>
2024-08-23 23:11:44 +0700

    first commit

---
+++ src/main/java/com/example/thang_sample_code/ThangSampleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.thang_sample_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThangSampleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThangSampleCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thang_sample_code/ThangSampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thang_sample_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThangSampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
